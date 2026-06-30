9d41b7531cb1086866203f740226d3137ab3ab1e
bharat roy <bharatroy1005@gmail.com>
2026-06-30 12:51:43 +0600

    User Table into data insert

---
+++ insert.sql
@@ -0,0 +1,8 @@
+-- Insert users table
+INSERT INTO users (full_name, email, role, phone_number)
+VALUES
+    ('Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+    ('Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+    ('Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+    ('Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL),
+    ('Nusrat Jahan', 'nusrat@mail.com', 'Football Fan', '+8801744444444');
