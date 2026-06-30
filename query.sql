9e34b042cd9271422abc6d04088f66d155c34551
bharat roy <bharatroy1005@gmail.com>
2026-06-30 15:38:11 +0600

    All table query is complited

--- query.sql
+++ query.sql
@@ -30,3 +30,46 @@ FROM
   bookings
 WHERE
   payment_status IS NULL;
+
+--   QUERY-4
+SELECT
+  booking_id,
+  full_name,
+  fixture,
+  total_cost
+FROM
+  bookings b
+  INNER JOIN users u ON b.user_id = u.user_id
+  INNER JOIN matches m ON b.match_id = m.match_id;
+
+--   QUERY-5
+
+SELECT 
+  u.user_id, u.full_name, b.booking_id 
+FROM users u
+ LEFT JOIN bookings b
+ON u.user_id = b.user_id;
+
+-- QUERY-6
+
+SELECT
+  booking_id,
+  match_id,
+  total_cost
+FROM 
+  bookings 
+WHERE total_cost >(
+  SELECT avg(total_cost) 
+  FROM bookings
+) 
+
+-- QUERY-7
+
+SELECT
+match_id, 
+  fixture,
+  base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+LIMIT 2
+OFFSET 1;
