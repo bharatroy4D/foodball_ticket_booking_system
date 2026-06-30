4f826821a0ce1d8cf84600ab1ca3cca29646ec56
bharat roy <bharatroy1005@gmail.com>
2026-06-30 12:49:19 +0600

    Bookings Table is created

--- schema.sql
+++ schema.sql
@@ -8,12 +8,26 @@ CREATE TABLE users (
     phone_number VARCHAR(20)
 );
 
-  -- Matches table
-  CREATE TABLE matches(
-  match_id INT primary key,
+  -- Matchs table
+  CREATE TABLE matchs(
+  match_id INT PRIMARY KEY,
   fixture VARCHAR(150) NOT NULL,
   tournament_category VARCHAR(100),
   base_ticket_price DECIMAL(10,2),
   match_status VARCHAR(50)
   CHECK(match_status IN('Availabe', 'Selling Fast', 'sold Out', 'postponded'))
   );
+
+  -- Bookings Table
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    match_id INT REFERENCES matches(match_id),
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20)
+        CHECK (
+            payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+            OR payment_status IS NULL
+        ),
+    total_cost DECIMAL(10,2)
+);
